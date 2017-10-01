f65c834d5e763288a17bca322b08c4c29bb7095a
Dan Felicetta <felicetta@betterment.com>
2017-10-01 14:30:24 -0400

    guest edit

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -2,4 +2,24 @@ class GuestsController < ApplicationController
   def index
     @guests = Guest.all
   end
+
+  def edit
+    @guest = Guest.find params[:id]
+  end
+
+  def update
+    @guest = Guest.find params[:id]
+
+    if @guest.update update_params
+      redirect_to invitation_path(@guest.invitation)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def update_params
+    params.fetch(:guest, {}).permit(:name)
+  end
 end
