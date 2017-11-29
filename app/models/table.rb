033e0617af6d645ff3f4d21060b140d70bbc4eca
Dan Felicetta <felicetta@betterment.com>
2017-11-29 16:10:22 -0500

    display entree counts

--- app/models/table.rb
+++ app/models/table.rb
@@ -4,4 +4,19 @@ class Table < ApplicationRecord
   def full_name
     "(#{number}) - #{description}"
   end
+
+  Guest::ENTREE_CHOICES.each do |entree|
+    define_method "count_#{entree}" do
+      guests.where(entree_selection: entree).count
+    end
+  end
+
+  def entree_counts_display
+    s = ""
+    Guest::ENTREE_NAMES.each do |entree, display_name|
+      count = send(:"count_#{entree}")
+      s += "#{count} #{display_name} "
+    end
+    s.strip
+  end
 end
